ca5da0fa8971dc4bea59218181652da683a87515
Ossi Korpela <ossi.korpela@tuni.fi>
2024-11-04 16:21:44 +0200

    ex4 done

--- service1/service1.py
+++ service1/service1.py
@@ -3,6 +3,12 @@ from flask import Flask, jsonify
 
 import subprocess
 import requests
+import time
+
+import threading
+
+lock = threading.Lock()
+unavailable_until = 0
 
 
 app = Flask(__name__)
@@ -20,15 +26,29 @@ def sys_info():
         "Uptime": uptime
     }
 
+
 @app.route('/', methods=['GET'])
 def index():
-    info1 = sys_info()
-    response2 = requests.get('http://service2:8200/')
-    info2 = response2.json()
-    return jsonify({
-        "Service1": info1,
-        "Service2": info2
-    })
+    global unavailable_until
+    current_time = time.time()
+    if current_time < unavailable_until:
+        return jsonify({"error": "busy"}), 503
+    
+    with lock:
+        info1 = sys_info()
+        response2 = requests.get('http://service2:8200/')
+        info2 = response2.json()
+        
+        resp = jsonify({
+            "Service1": info1,
+            "Service2": info2
+        })
+
+        
+        unavailable_until = time.time() + 2      
+        return resp
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8199)
